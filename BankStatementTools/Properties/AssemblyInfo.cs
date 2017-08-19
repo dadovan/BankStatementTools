8dbf505d2916a5f4e17a489a0d07dda2cc39c7c6
Donovan Smith <dadovan@live.com>
2017-08-19 11:16:15 -0700

    Initial commit for verification

---
+++ BankStatementTools/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BankStatementTools")]
+[assembly: AssemblyProduct("BankStatementTools")]
+[assembly: AssemblyCopyright("Copyright © 2017 Donovan Smith")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("fe568249-9525-4b7c-bac2-ffd0b6dd1e28")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
